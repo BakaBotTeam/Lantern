dd67e21a7a9e96a3b165cb9b3f4dfdc840d2ccfc
guimc <xiluo@guimc.ltd>
2023-08-21 00:15:36 +0800

    !?..! <3

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AutoPixelParty.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AutoPixelParty.kt
@@ -8,12 +8,14 @@ import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.ClientUtils
+import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.minecraft.block.Block
 import net.minecraft.block.BlockColored
 import net.minecraft.item.ItemBlock
 import net.minecraft.network.play.server.S2FPacketSetSlot
 import net.minecraft.util.BlockPos
+import net.minecraft.util.MathHelper
 import net.minecraft.util.Vec3
 
 @ModuleInfo(name = "AutoPixelParty", spacedName = "Auto PixelParty", description = "PixelParty (Only hypixel)", category = ModuleCategory.MISC)
@@ -29,14 +31,15 @@ class AutoPixelParty: Module() {
     @EventTarget
     fun onUpdate(e: UpdateEvent) {
         if (mc.thePlayer != null && mc.theWorld != null && mc.thePlayer.inventory.getCurrentItem().item is ItemBlock && targetPosX == null) {
-            val targetBlockColored = mc.thePlayer.inventory.getCurrentItem().metadata
+            val targetBlockColored = mc.thePlayer.inventory.getCurrentItem()
             ClientUtils.displayChatMessage("search target pos")
             var distance = Double.MAX_VALUE
             for (x in -33..33) {
                 for (z in -33..33) {
                     val scannedBlockState = mc.theWorld.getBlockState(BlockPos(x, 0, z))
-                    if (targetBlockColored == scannedBlockState.block.getMetaFromState(scannedBlockState)) {
-                        val ndistance = mc.thePlayer.getDistance(x.toDouble() + 0.5, 1.5, z.toDouble() + 0.5)
+                    if (targetBlockColored.metadata == scannedBlockState.block.getMetaFromState(scannedBlockState) &&
+                            (targetBlockColored.item as ItemBlock).block == scannedBlockState.block) {
+                        val ndistance = mc.thePlayer.getDistance(x.toDouble() + 0.5, mc.thePlayer.posY, z.toDouble() + 0.5)
                         if (ndistance <= distance) {
                             distance = ndistance
                             targetPosX = x
@@ -51,15 +54,22 @@ class AutoPixelParty: Module() {
 
     @EventTarget
     fun onRender2D(e: Render2DEvent) {
-        val targetBlockColored = mc.thePlayer.inventory.getCurrentItem().metadata
         if (targetPosX != null && mc.thePlayer != null) {
-            val blockStateUnder = mc.theWorld.getBlockState(BlockPos(mc.thePlayer.posX.toInt(), 0, mc.thePlayer.posZ.toInt()))
-            if ((targetBlockColored == blockStateUnder.block.getMetaFromState(blockStateUnder)) || mc.thePlayer.getDistance(targetPosX!!.toDouble() + 0.5, 1.5, targetPosZ!!.toDouble() + 0.5) <= 0.7) {
-                mc.gameSettings.keyBindForward.pressed = false
+            if (mc.thePlayer.getDistance(targetPosX!!.toDouble() + 0.5, mc.thePlayer.posY, targetPosZ!!.toDouble() + 0.5) <= 0.25) {
+                // mc.gameSettings.keyBindForward.pressed = false
                 return
             }
-            RotationUtils.toRotation(Vec3(targetPosX!!.toDouble()+0.5, 1.5, targetPosZ!!.toDouble()+0.5), false).toPlayer(mc.thePlayer!!)
-            mc.gameSettings.keyBindForward.pressed = true
+            val targetRot = RotationUtils.toRotation(Vec3(targetPosX!!.toDouble()+0.5, mc.thePlayer.posY, targetPosZ!!.toDouble()+0.5), false)
+            RotationUtils.setTargetRotation(targetRot, 1)
+            // mc.gameSettings.keyBindForward.pressed = true
+            val (yaw) = targetRot
+
+            val yawSin = MathHelper.sin((yaw * Math.PI / 180F).toFloat())
+            val yawCos = MathHelper.cos((yaw * Math.PI / 180F).toFloat())
+            val baseMoveSpeed = MovementUtils.getBaseMoveSpeed()
+
+            mc.thePlayer.motionX += baseMoveSpeed * yawCos - baseMoveSpeed * yawSin
+            mc.thePlayer.motionZ += baseMoveSpeed * yawCos + baseMoveSpeed * yawSin
         }
     }
 
@@ -70,18 +80,6 @@ class AutoPixelParty: Module() {
             mc.gameSettings.keyBindForward.pressed = false
             targetPosX = null
             targetPosZ = null
-        }
-    }
-
-    fun equalsBlock(b1: Block, b2: Block): Boolean {
-        if (b1 == b2) {
-            if (b1 is BlockColored && b2 is BlockColored) {
-                return b1.blockColor == b2.blockColor
-            } else {
-                return true
-            }
-        } else {
-            return false
         }
     }
 }
